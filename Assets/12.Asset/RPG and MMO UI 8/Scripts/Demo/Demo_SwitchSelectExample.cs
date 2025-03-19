1139b8994c2abc215fce194e8c0fe809f8918815
nosoeugyub <74639167+nosoeugyub@users.noreply.github.com>
2025-03-19 16:48:47 +0900

    파일 분할 업로드

---
+++ Assets/12.Asset/RPG and MMO UI 8/Scripts/Demo/Demo_SwitchSelectExample.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace DuloGames.UI
+{
+    public class Demo_SwitchSelectExample : MonoBehaviour
+    {
+        [SerializeField] private UISwitchSelect m_SwitchSelect;
+
+        private void OnEnable()
+        {
+            if (this.m_SwitchSelect != null)
+                this.m_SwitchSelect.onChange.AddListener(OnOptionChange);
+        }
+
+        private void OnDisable()
+        {
+            if (this.m_SwitchSelect != null)
+                this.m_SwitchSelect.onChange.RemoveListener(OnOptionChange);
+        }
+
+        public void OnOptionChange(int index, string option)
+        {
+            Debug.Log("Switch Select option changed to: " + index + " " + option);
+        }
+    }
+}
